accc185b468f3570da05a677c9194007047db6f9
Git Global Folder <sajeeb126599@gmail.com>
2022-06-25 11:23:16 +0600

    props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import MyFunction from "./components/Card";
 function App(){
     return <div>
         <h1 className="headingStyle"> Todo App</h1>
-        <MyFunction />
-        <MyFunction />
-        <MyFunction />
+        <MyFunction  todoTitle = "Call Mother" todoDesc= "This is desc1"/>
+        <MyFunction  todoTitle = "Call Father" todoDesc= "This is desc2"/>
+        <MyFunction  todoTitle = "Call Brother" todoDesc= "This is desc2"/>
     </div>
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
-//const todoTitle = "Call Family";
-//const todoDesc = "Elitr est magna sed ipsum et et ipsum. Labore rebum amet accusam tempor gubergren et no. Consetetur et ipsum dolor.";
+// const todoTitle = "Call Family";
+// const todoDesc = "Elitr est magna sed ipsum et et ipsum. Labore rebum amet accusam tempor gubergren et no. Consetetur et ipsum dolor.";
 const current = new Date();
   const Crntdate = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
 
-  function MyFunction(){
+  function MyFunction(props){
     return  <div className='Card'>
-                      <h3 className='cardTitle'> {todoTitle}</h3>
-                      <p className='cardDesc'>{todoDesc}</p>
+                      <h3 className='cardTitle'> {props.todoTitle}</h3>
+                      <p className='cardDesc'>{props.todoDesc}</p>
                       <p className='cardFooter'>{Crntdate}</p>
                 </div>
   }
